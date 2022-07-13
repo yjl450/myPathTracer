a397113deeead3603e7f668b7537af3ca6f4ece6
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2022-07-13 21:04:23 +0800

    init

---
+++ myPathTracer.cpp
@@ -0,0 +1,18 @@
+﻿
+#include "myPathTracer.h"
+
+
+int main(int argc, char** argv)
+{
+	std::cout << "Simple Path Tracer v0.1\nBy Yijian Liu" << std::endl;
+	if (argc != 2) {
+		std::cout << "\nOne argument needed for scene description." << std::endl;
+		return 0;
+	}
+	std::ifstream scenefile(argv[1], std::ios::in);
+	assert(scenefile.is_open());
+	std::cout << "\n" << argv[1] << " load successfully" << std::endl;
+
+
+	return 0;
+}
---
+++ myPathTracer.h
@@ -0,0 +1,9 @@
+﻿// myPathTracer.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
+#include <sstream>
