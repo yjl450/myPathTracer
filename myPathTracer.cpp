884b39a4b6a1f03928a5c1b778fe33c70ac329b8
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2022-07-17 18:50:40 +0800

    add fpng

--- myPathTracer.cpp
+++ myPathTracer.cpp
@@ -1,18 +1,73 @@
 ﻿
 #include "myPathTracer.h"
 
+using namespace std;
+
+vector<double> read_vals(stringstream &s, int num) {
+	string val;
+	vector<double> vals;
+	for (int i = 0; i < num; i++) {
+		s >> val;
+		vals.push_back(stod(val));
+	}
+	return vals;
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Simple Path Tracer v0.1\nBy Yijian Liu" << std::endl;
+	cout << "Simple Path Tracer v0.1\nBy Yijian Liu" << endl;
 	if (argc != 2) {
-		std::cout << "\nOne argument needed for scene description." << std::endl;
+		cerr << "\nOne argument needed for scene description." << endl;
 		return 0;
 	}
-	std::ifstream scenefile(argv[1], std::ios::in);
-	assert(scenefile.is_open());
-	std::cout << "\n" << argv[1] << " load successfully" << std::endl;
+	ifstream scenefile(argv[1], ios::in);
+	if (!scenefile.is_open()) {
+		cerr << "Cannot open scene description file." << endl;
+	} else {
+		cout << "\nParsing " << argv[1] << endl; 
+	}
 
+	// start parsing scene description
+	Scene scene{0};
+	string parseline;
+	string cmd;
+	vector<double> vals;
+	stringstream s;
+
+	scene.maxdepth = 5; // default maxdepth if not provided
+	while (getline(scenefile, parseline)) {
+		if (parseline[0] == '#' || parseline.length() == 0) {
+			continue;
+		}
+		s.clear();
+		s.str(parseline);
+		s >> cmd;
+		if (cmd == "size"){
+			s >> scene.width;
+			s >> scene.height;
+		} else if (cmd == "camera") {
+			vals = read_vals(s, 10);
+			scene.cameraFrom << vals[0], vals[1], vals[2];
+			scene.cameraAt << vals[3], vals[4], vals[5];
+			scene.cameraUp << vals[6], vals[7], vals[8];
+			scene.fov = vals[9];
+		}
+		cout << "\t" << parseline << endl;
+	}
+
+	// start shading
+	unsigned char* canvas = new unsigned char[scene.height*scene.width*3];
+	int x = 0, y = 0;
+	for (int i = 0; i < scene.height * scene.width * 3; i+=3) {
+		y = i / 3 / scene.width;
+		x = (i / 3) - (i / 3 / scene.width) * scene.width;
+		canvas[i] = x * 127 / scene.width + y * 127 / scene.height;
+		canvas[i+1] = x * 127 / scene.width + y * 127 / scene.height;
+		canvas[i+2] = x * 127 / scene.width + y * 127 / scene.height;
+	}
+	fpng::fpng_init();
+	fpng::fpng_encode_image_to_file("output.png", canvas, scene.width, scene.height, 3);
+	delete[] canvas;
 
 	return 0;
 }
--- myPathTracer.h
+++ myPathTracer.h
@@ -1,9 +1,29 @@
-﻿// myPathTracer.h: 标准系统包含文件的包含文件
-// 或项目特定的包含文件。
+﻿#pragma once
 
-#pragma once
 #include <iostream>
 #include <fstream>
 #include <cassert>
 #include <string>
 #include <sstream>
+#include <vector>
+#include <Eigen/core>
+// fpng at https ://github.com/richgel999/fpng
+#include "fpng.h"
+#include "fpng.cpp"
+
+//typedef struct {
+//	Eigen::Vector3d
+//} Camera;
+
+typedef struct {
+	// canvas size
+	int width;
+	int height;
+	// max number of ray bounce
+	int maxdepth;
+	// camera setting
+	Eigen::Vector3d cameraFrom;
+	Eigen::Vector3d cameraAt;
+	Eigen::Vector3d cameraUp;
+	double fov;
+}Scene;
